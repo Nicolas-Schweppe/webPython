95639c5b38e6b6ce87354f4627ea1f235312d88c
nicolas-schweppe <nicolas_vialidad@hotmail.com>
2023-06-18 12:36:58 -0300

    se agrego conexion a base de datos

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask , render_template , url_for
+from flask import Flask , render_template ,request , redirect
+from flaskext.mysql import MySQL
+from datetime import date
+
 
 app=Flask(__name__)
+mysql=MySQL()
+
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='portafolio'
+mysql.init_app(app)
 
 @app.route('/')
 def inicio():
     return render_template('sitio/index.html')
+
+@app.route('/sitio/contacto/mensaje',methods=['POST'])
+def mensajeContacto():
+    _nombre = request.form['nombre']
+    _email = request.form['email']
+    _telefono= request.form['telefono']
+    _mensaje= request.form['mensaje']
+
+    hora_actual = date.today()
+    print(hora_actual)
+    sql="INSERT INTO `mensajes`(`idMensaje`, `nombre`, `correo`, `telefono`, `mensaje`, `fecha`) VALUES ('2',%s,%s,%s,%s,%s);"
+    datos=(_nombre,_email,_telefono,_mensaje,hora_actual)
+    conexion=mysql.connect()
+    cursor=conexion.cursor()
+    cursor.execute(sql,datos)
+    conexion.commit()
+
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
